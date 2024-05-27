6c6e77a74cb5ab5b742bf68ed2137b23528254a0
DESKTOP-6ELRJLQ\USER <knyatom@daum.net>
2024-05-27 18:18:21 +0900

    0527

--- shop2/shop2/src/main/java/com/shop2/controller/ThymeleafExController.java
+++ shop2/shop2/src/main/java/com/shop2/controller/ThymeleafExController.java
@@ -15,4 +15,8 @@ public class ThymeleafExController {
   return "thymeleafEx/thymeleafEx01";
  }
 
+ @GetMapping(value = "/ex07")
+ public String thymeleafExample07(){
+  return "thymeleafEx/thymeleafEx07";
+ }
 }
---
+++ shop2/shop2/src/main/java/com/shop2/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.shop2.repository;
+
+import com.shop2.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+ Member findByEmail(String email);
+}
+
+
+
